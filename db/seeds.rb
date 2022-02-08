40933a4de7e3b112c59628a134f5e3425d13f868
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-02-08 21:36:09 +0100

    add seed in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create!([{
+    name: "Daytime Place",
+    address: "6493 Beth Parkways, Emorystad",
+    phone_number: "202-555-0139",
+    category: "chinese"
+  },
+
+  {
+    name: "Easy Eats",
+    address: " Rue du Val, London",
+    phone_number: "202-555-0146",
+    category: "french"
+  },
+
+  {
+    name: "Cheerful Rhino",
+    address: "Rue de Beausoleil, Paris",
+    phone_number: "202-555-0166",
+    category: "japanese"
+  },
+
+  {
+    name: "Fare & Feed",
+    address: "La Barillerie, Paris",
+    phone_number: "202-555-0152",
+    category: "italian"
+  },
+
+  {
+    name: "Fast & Friendly",
+    address: "La Flèche, Paris",
+    phone_number: "202-555-0196",
+    category: "chinese"
+  },])
