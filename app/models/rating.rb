3e207344d5583fae9b00a87d23c8c95bb920ea87
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-02-08 21:12:15 +0100

    db and validates OK

--- app/models/rating.rb
+++
@@ -1,6 +0,0 @@
-class Rating < ApplicationRecord
-  belongs_to :restaurant
-
-  validates :star, :content, presence: true
-  validates :star, :inclusion => 0..5
-end
--- db/migrate/20220208193503_add_star_to_ratings.rb
+++
@@ -1,5 +0,0 @@
-class AddStarToRatings < ActiveRecord::Migration[6.1]
-  def change
-    add_column :ratings, :star, :integer
-  end
-end
