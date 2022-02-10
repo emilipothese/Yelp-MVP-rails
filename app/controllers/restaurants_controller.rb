23eedf9ccd7a6b3a7c972ec65b18ae551332d7d8
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-02-10 19:25:17 +0100

    Rails OK mais les reviews ne s'affichent pas

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,32 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
 
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
